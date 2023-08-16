e9275365c3fe065a059ea083ae56a4ee5a2ee647
faritg1 <juanagelves@gmail.com>
2023-08-16 13:42:18 -0500

    promptListo

---
+++ js/16-08-2023/main.js
@@ -0,0 +1,12 @@
+/* EJERCICIO 1 */
+function fnSecuencia(num) {
+    let cont = " "
+    for (let i = 1; i < num; i++) {
+        cont += i + " "
+        console.log(cont);
+    }
+}
+
+fnSecuencia(20)
+
+/* CARLOS FARIT TORRES GELVES */    
--- js/estructuraDeDatos/main.js
+++ js/estructuraDeDatos/main.js
@@ -1,3 +1,4 @@
+/* PILA CON ARRAY */
 class Stack{
     constructor(){
         this.items=[]
@@ -25,26 +26,65 @@ class Stack{
         }
         return this.items.pop();
     }
+
     peek(){//Revisar el primer dato encontrado
         if(this.isEmpty()){
             return "La pila esta vacia"
         }
         return this.items[this.items.length - 1];
-
     }
-
 }
 
-const stack = new Stack();
+const Stack = new Stack();
 
-stack.push(10);
-stack.push(20);
-stack.push(30);
+Stack.push(10);
+Stack.push(20);
+Stack.push(30);
 
-console.log(stack.peek())
-stack.pop()
-console.log(stack.peek())
-stack.pop()
-console.log(stack.peek())
-stack.clear()
-console.log(stack.peek())
+console.log(Stack.peek())
+Stack.pop()
+console.log(Stack.peek())
+Stack.pop()
+console.log(Stack.peek())
+Stack.clear()
+console.log(Stack.peek())
+
+/* PILA CON NODE */
+class node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class stack{
+    constructor(){
+        this.top = null
+        this.size = 0
+    }
+
+    getSize(){
+        return this.size;
+    }
+
+    isEmpty(){
+        return this.size===0;
+    }
+
+    push(value){
+        let newNode = new node(value)
+        newNode.next = this.top
+        this.top = newNode
+        this.size++
+    }
+
+    pop(){
+        if (!this.top){
+            return "Is Empty"
+        }
+        const poppedValue = this.top.value;
+        this.top = this.top.next;
+        this.size--;
+        return this.top.value;
+    }
+}
