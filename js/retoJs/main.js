423714e4ead07f7e340bb1b3bfb45193a3d2d617
faritg1 <juanagelves@gmail.com>
2023-08-16 13:39:50 -0500

    prompt

---
+++ js/retoJs/main.js
@@ -0,0 +1,21 @@
+/* EJERCICIO 2 */
+const prompt = require("prompt-sync")({sigint:true});
+let passONE = prompt("Ingrese algo one :")
+let passTWO = prompt("Ingrese algo two :")
+
+function hash(passOne, passTwo){
+    let hash = "";
+    for (let i = 0; i <= passOne.length; i++) {
+        if(i < passOne.length){
+            hash +=  passOne[i]
+        }
+        for (let j = i; j <= passTwo.length; j++) {
+            if(j < passTwo.length){
+                hash +=  passTwo[j]
+                break
+            }
+        }
+    }
+    return hash
+}
+console.log(hash(passONE,passTWO))
